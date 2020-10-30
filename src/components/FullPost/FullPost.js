70fdd50978b9ebd9465f7f15f14b8f0567d55171
Ivan Cox <ivn.cox@gmail.com>
2020-10-30 06:29:33 -0400

    Adding and understanding axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,50 @@
-import React, { Component } from 'react';
-
-import './FullPost.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+  state = {
+    post: null,
+  };
 
-        );
-        return post;
+  componentDidUpdate() {
+    if (this.props.id) {
+      if (
+        !this.state.post ||
+        (this.state.post && this.state.post.id !== this.props.id)
+      ) {
+        axios.get("/posts/" + this.props.id).then((response) => {
+          this.setState({ post: response.data });
+        });
+      }
     }
+  }
+
+  deletePostHandler = () => {
+    axios.delete("/posts/" + this.props.id).then((response) => {
+      console.log(response);
+    });
+  };
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = <p style={{ textAlign: "center", color: "red" }}>LOADING...!</p>;
+    }
+    if (this.state.post) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.post.title}</h1>
+          <p>{this.state.post.body}</p>
+          <div className="Edit">
+            <button className="Delete" onClick={this.deletePostHandler}>
+              Delete
+            </button>
+          </div>
+        </div>
+      );
+    }
+    return post;
+  }
 }
 
 export default FullPost;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+
+/* axios interceptors are fired for every request 
+sent throughtout the application and can be used to 
+set headers in the application
+*/
+
+// interceptor for sending requests
+// Can be used for internet connectivity errors
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    // must return the request or you will recieve errors
+    return request;
+  },
+  (error) => {
+    // Error handling can be used for a log file
+    console.log(error);
+    // pass the error unto the component which handles it
+    return Promise.reject(error);
+  }
+);
+
+// interceptor for handling responses
+// usued when there is an error with the response
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    // must return the response or you will recieve errors
+    return response;
+  },
+  (error) => {
+    // Error handling can be used for a log file
+    console.log(error);
+    // pass the error unto the component which handles it
+    return Promise.reject(error);
+  }
+);
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
