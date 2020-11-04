7fd25384428c409ca86373e666980fccb1708791
Ivan Cox <ivn.cox@gmail.com>
2020-11-04 07:53:17 -0400

    Add routing by using react-router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,68 +1,38 @@
 import React, { Component } from "react";
-import axios from "axios";
-
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
+import {Route, Switch, Link, BrowserRouter as Router} from "react-router-dom";
 import "./Blog.css";
+import Posts from "./Posts/Posts";
+import NewPost from "../../containers/Blog/NewPost/NewPost";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false,
-  };
-
-  componentDidMount() {
-    axios
-      .get("/posts")
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
-          return {
-            ...post,
-            author: "Ivan",
-          };
-        });
-        this.setState({ posts: updatedPosts });
-      })
-      .catch((error) => {
-        this.setState({ error: true });
-      });
-  }
-
-  postClickedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  };
-
   render() {
-    let posts = (
-      <p style={{ textAlign: "center", color: "Red", fontWeight: "bold" }}>
-        There was an issue Loading Posts
-      </p>
-    );
-    if (!this.state.error) {
-      posts = this.state.posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postClickedHandler(post.id)}
-          />
-        );
-      });
-    }
     return (
-      <div>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
+      <Router>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to={{
+                  pathname: '/new-post',
+                }}>New Post</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Switch>
+        <Route exact path="/">
+          <Posts />
+        </Route>
+          <Route exact path="/new-post">
           <NewPost />
-        </section>
+        </Route>
+        </Switch>
       </div>
+      </Router>
     );
   }
 }
